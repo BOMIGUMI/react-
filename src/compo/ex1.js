3b725a09ebd0a6c5d5d17880317d2da55a7245d3
Saebom Kim <rlatoqha0101@naver.com>
2022-11-06 17:57:48 +0900

    컴포넌트

---
+++ src/compo/ex1.js
@@ -0,0 +1,10 @@
+
+function Ex1(){
+    return(
+        <>
+            <h1>첫번째 컴포</h1>
+        </>
+    )
+}
+
+export default Ex1;
---
+++ src/compo/ex2.js
@@ -0,0 +1,12 @@
+
+
+function Ex2(){
+    return(
+
+    <>
+        <h1>두번째 컴포</h1>
+    </>
+    )
+}
+
+export default Ex2;
